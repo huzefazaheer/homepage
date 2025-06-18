4460babefe2ef96b5ddb4babeba5cd59f86162e1
Huzefa Zaheer <huzefazaheer1@gmail.com>
2025-06-19 00:27:22 +0500

    Added light animations on homepage

--- index.js
+++ index.js
@@ -20,5 +20,44 @@ function toggleCard(e){
         CARDHEADING.classList.toggle("hide")
         CARDP.classList.toggle("hide")
         CARD.classList.toggle("small")
+        CARD.classList.toggle("large")
     }
 }
+
+
+const shape1 = document.querySelector(".c1")
+const shape2 = document.querySelector(".c2")
+
+let c1posX = 102
+let c1posY = 0
+let c2posX = 402
+let c2posY = 80
+let c1reset = false
+let c2reset = false
+let i = 0
+function animate(){
+    
+    if(i > 100) {
+        c1posX -= 4
+        c1posY -= 4
+        c2posX += 4
+        c2posY += 4
+    }else{
+        c1posX += 4
+        c1posY += 4
+        c2posX -= 4
+        c2posY -= 4
+    }
+    if(i==200) i=0
+    i++
+
+    shape1.style.left = c1posX +"px"
+    shape1.style.top = c1posY +"px"
+    shape2.style.left = c2posX +"px"
+    shape2.style.top = c2posY +"px"
+    i++
+}
+
+// if(i!=20){
+    setInterval(animate, 100)
+// }
